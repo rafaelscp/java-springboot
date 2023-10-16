16be6a05f2e79b4d85949e3fb03c6bb25812b709
rafaelscp <faeel-xd@hotmail.com>
2023-10-15 23:28:26 -0300

    Dockerfile deploy

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubunt:latest AS build
+
+RUN apt-get update
+RUN apt-get install openjdk-170jdk -y
+
+COPY . .
+
+RUN apt-get install maven -y
+RUN mvn clean install
+
+FROM openjdk:17-jdk-slim
+
+EXPOSE 8080
+
+COPY --from=build /target/todolist-1.0.0.jar app.jar
+
+ENTRYPOINT [ "java", "-jar", "app.jar"]
---
+++ src/main/java/br/com/rafaelcarvalho/todolist/user/UserController.java
@@ -0,0 +1,55 @@
+package br.com.rafaelcarvalho.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private IUserRepository userRepository;
+
+    /**
+     * String 
+     * Integer
+     * Double
+     * Float
+     * char 
+     * Date
+     * void
+     */
+    /*
+     * Body
+     */
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            //Mensagem de erro
+            // Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+}
